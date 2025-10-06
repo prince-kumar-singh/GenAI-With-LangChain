984ee25fd8e7a513c0d0a3dfe7c96b1ed7263646
Prince Kumar Singh <138196541+prince-kumar-singh@users.noreply.github.com>
2025-10-06 19:58:56 +0530

    Add WebBaseLoader implementation for loading product information using LangChain and Google Generative AI

---
+++ 07langchain-document-loaders/04webbase_loader.py
@@ -0,0 +1,25 @@
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-pro", temperature=0)
+prompt = PromptTemplate(
+    template='Answer the following question \n {question} from the following text - \n {text}',
+    input_variables=['question','text']
+)
+
+parser = StrOutputParser()
+
+url = 'https://www.flipkart.com/acer-aspire-3-intel-celeron-dual-core-8-gb-256-gb-ssd-windows-11-home-a311-45-thin-light-laptop/p/itmad38595c5a902'
+loader = WebBaseLoader(url)
+
+docs = loader.load()
+
+
+chain = prompt | model | parser
+
+print(chain.invoke({'question':'What is the prodcut that we are talking about?', 'text':docs[0].page_content}))
